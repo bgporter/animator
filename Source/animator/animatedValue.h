5d1ec7c69c4209089a90f257dab7249f86822aa3
Brett g Porter <bgp@bgporter.net>
2019-08-26 16:46:50 -0400

    Add Linear Animated Value class (empty)

--- Source/animator/animatedValue.h
+++ Source/animator/animatedValue.h
@@ -1,11 +1,103 @@
 /*
-  ==============================================================================
-
-    animatedValue.h
-    Created: 23 Aug 2019 12:21:19pm
-    Author:  Brett Porter
-
-  ==============================================================================
-*/
+ * Copyright (c) 2019 Brett g Porter. All Rights Reserved.
+ */
 
 #pragma once
+
+#include "../../JuceLibraryCode/JuceHeader.h"
+
+
+class AnimatedValue 
+{
+public:
+   /**
+    * Base class init for the animated value classes. 
+    * @param startVal  Initial Value 
+    * @param endVal    Target/end value. 
+    * @param tolerance Toleerance for when we've reached the target value.
+    */
+   AnimatedValue(float startVal, float endVal, float tolerance)
+   :  fStartVal(startVal)
+   ,  fEndVal(endVal)
+   ,  fTolerance(tolerance)
+   ,  fCurrentVal(startVal)
+   ,  fLastTime(-1)
+   {
+      
+   };
+   
+   virtual ~AnimatedValue()
+   {
+      
+   };
+   
+   /**
+    * Derived classes should do whatever is needed to generate and 
+    * return the next value. 
+    * @param  msTime current time in ms. 
+    * @return        next value (or last value if we're finished)
+    */
+   float GetNextValue(int msTime)
+   {
+      if (-1 == fLastTime)
+      {
+         fCurrentVal = fStartVal;
+         fLastTime = msTime;
+      }
+      else 
+      {
+         fCurrentVal = this->GenerateNextValue(msTime - fLastTime);
+         fLastTime = msTime;
+      }
+      return fCurrentVal;
+   }
+   
+   /**
+    * Have we reached the end of this animation sequence? By default, 
+    * we're done when the current value is within `fTolerance` of the endValue.
+    * @return [description]
+    */
+   virtual bool IsFinished()
+   {
+      return (std::fabs(fCurrentVal - fEndVal) < fTolerance);
+   }
+   
+   /**
+    * Reset the object so it can run again using the same start/end values.
+    */
+   void Reset()
+   {
+      fLastTime = -1;
+      this->DoReset();
+   }
+
+
+private:
+   
+   /**
+    * Implemented in base classes to generate the next value in the sequence. 
+    * @param  time ms since the last time we were called. 
+    * @return      next value.
+    */
+   virtual float GenerateNextValue(int deltaTime) = 0;
+   
+   /**
+    * Override in derived classes to perform any additional reset of calculations. 
+    */
+   virtual void DoReset()
+   {
+      
+   }
+
+
+protected: 
+   float fStartVal;
+   float fEndVal;
+   float fTolerance;
+   float fCurrentVal;
+   
+   int fLastTime;
+   
+private:
+   
+};
--- Source/animator/animationController.h
+++ Source/animator/animationController.h
@@ -1,11 +1,45 @@
 /*
-  ==============================================================================
-
-    animationController.h
-    Created: 23 Aug 2019 12:20:42pm
-    Author:  Brett Porter
-
-  ==============================================================================
-*/
-
+ * Copyright (c) 2019 Brett g Porter. All Rights Reserved.
+ */
 #pragma once
+
+#include "../../JuceLibraryCode/JuceHeader.h"
+
+
+class AnimatedValue;
+
+template <std::size_t valueCount>
+class AnimationController 
+{
+public:
+   AnimationController()
+   {
+      
+   }
+   
+   /**
+    * [Update description]
+    * @param  msTime [description]
+    * @return        [description]
+    */
+   int Update(int msTime)
+   {
+      return 0;
+   }
+   
+   
+   
+private:
+   
+   std::array<std::unique_ptr<AnimatedValue>, valueCount> fValues;
+   
+   /// function to call on each frame. Pass in std::array of new values, 
+   /// return true if all is okay, false to cancel this animation. 
+   std::function<bool(std::array<float, valueCount>)> fUpdateFn;
+   
+   /// function to call when the animation is completed or canceled. 
+   std::function<void(void)> fCompleteFn;
+   
+   
+   
+};
