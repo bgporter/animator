339b37dd5ce056d91cd5b498c0aac1a8e0f2faa3
Brett g Porter <bgp@bgporter.net>
2019-08-27 20:50:26 -0400

    Add slew value and test

---
+++ Source/animator/slewAnimatedValue.cpp
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2019 Brett g Porter. All Rights Reserved.
+ */
+
+#include "slewAnimatedValue.h"
+
+
+
+SlewAnimatedValue::SlewAnimatedValue(int startVal, int endVal, float tolerance, float slewRate)
+:  AnimatedValue(startVal, endVal, tolerance) 
+,  fSlewRate(slewRate)
+{
+   
+}
+
+float SlewAnimatedValue::GenerateNextValue(int deltaTime)
+{
+   return fCurrentVal + fSlewRate * (fEndVal - fCurrentVal);
+}
+
+#ifdef qRunUnitTests
+#include "test/test_SlewAnimatedValue.cpp"
+#endif
---
+++ Source/animator/slewAnimatedValue.h
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2019 Brett g Porter. All Rights Reserved.
+ */
+
+#pragma once
+
+#include "animatedValue.h" 
+
+class SlewAnimatedValue : public AnimatedValue 
+{
+public:
+   SlewAnimatedValue(int startVal, int endVal, float tolerance, float slewRate);
+   
+private:
+   float GenerateNextValue(int deltaTime) override;
+   
+private:
+   float fSlewRate;
+};
