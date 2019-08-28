78ee2647429a1cbef18760a5475df00f1e0ee237
Brett g Porter <bgp@bgporter.net>
2019-08-28 10:40:11 -0400

    Initial (untested) implementation of vector val.

--- Source/animator/vectorAnimatedValue.cpp
+++ Source/animator/vectorAnimatedValue.cpp
@@ -6,22 +6,49 @@
 VectorAnimatedValue::VectorAnimatedValue(int startVal, int endVal, float tolerance, 
    float accel, float damping)
 :  AnimatedValue(startVal, endVal, tolerance)
+,  fStartAcceleration(accel)
 ,  fAcceleration(accel)
 ,  fDamping(damping)
 ,  fVelocity(0)
 {
+   // the damping factor must be between 0..1. 
+   jassert(isPositiveAndBelow(damping, 1.f));
    
 }
 
 
 float VectorAnimatedValue::GenerateNextValue()
 {
+   float prev = fCurrentVal;
+   fVelocity += fAcceleration;
+   float next = prev + fVelocity;
+   
+   // see if we have crossed over the end value
+   // if so, we may need to change direction and dampen the oscillation
+   if ((fEndVal - prev) * (fEndVal - next) < 0)
+   {
+      if (0.f == fDamping)
+      {
+         // we overshot the end and don't want to oscillate. Clamp to the 
+         // end value instead. 
+         next = fEndVal;
+      }
+      else 
+      {
+         fAcceleration = -1 * fAcceleration * fDamping;
+      }
+   }
+   
+   return next;
+   
+   
    
 }
 
 void VectorAnimatedValue::DoReset()
 {
    fVelocity = 0;
+   fAcceleration = fStartAcceleration;
 }
    
    
--- Source/animator/vectorAnimatedValue.h
+++ Source/animator/vectorAnimatedValue.h
@@ -19,9 +19,15 @@ private:
    void DoReset() override;
    
 private:
+   /// The initial acceleration for this value. 
+   float fStartAcceleration;
+   
+   /// When we're in a damping state, the acceleration will change. 
    float fAcceleration;
    
+   /// damping factor to apply when we need to oscillate toward end val. 
    float fDamping;
    
+   /// current velocity, updated with acceleration every frame. 
    float fVelocity;
 };
