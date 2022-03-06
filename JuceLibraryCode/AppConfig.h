86d2be8c0e38316f4b116330ddf4471091c9bce9
Brett g Porter <brett@bgporter.net>
2022-03-05 21:39:33 -0500

    Correct sinusoid behavior when using start phase != 0.

--- JuceLibraryCode/AppConfig.h
+++ JuceLibraryCode/AppConfig.h
@@ -43,7 +43,7 @@
 
 #define JUCE_USE_DARK_SPLASH_SCREEN 1
 
-#define JUCE_PROJUCER_VERSION 0x60102
+#define JUCE_PROJUCER_VERSION 0x60105
 
 //==============================================================================
 #define JUCE_MODULE_AVAILABLE_friz                      1
